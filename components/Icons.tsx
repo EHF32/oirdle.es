54204f8c667fcf99979a3f2e80c1dd452ac1ad9f
iSh0Tx <javi2003pl@gmail.com>
2022-03-30 21:35:25 +0200

    :construction: :lipstick: :sparkles: Tailwind y main header

---
+++ components/Icons.tsx
@@ -0,0 +1,45 @@
+export function InfoIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-18A10 10 0 002 12a10 10 0 0010 10 10 10 0 0010-10A10 10 0 0012 2m-1 15h2v-6h-2v6z"></path>
+    </svg>
+  );
+}
+
+export function HeartIcon() {
+  return (
+    <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M12.1 18.55l-.1.1-.11-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5 22 5.41 19.58 3 16.5 3z"
+      ></path>
+    </svg>
+  );
+}
+
+export function StatsIcon() {
+  return (
+    <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M3 22V8h4v14H3m7 0V2h4v20h-4m7 0v-8h4v8h-4z"
+      ></path>
+    </svg>
+  );
+}
+
+export function HelpIcon() {
+  return (
+    <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M11 18h2v-2h-2v2m1-16A10 10 0 002 12a10 10 0 0010 10 10 10 0 0010-10A10 10 0 0012 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-14a4 4 0 00-4 4h2a2 2 0 012-2 2 2 0 012 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5a4 4 0 00-4-4z"
+      ></path>
+    </svg>
+  );
+}
