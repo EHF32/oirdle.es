38db11db4655c4e57db283364231b37fd8ee3e8b
Ángel <angel6v3@gmail.com>
2022-09-24 15:56:16 +0200

    20220924

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import { CSSObjectWithLabel, Options } from "react-select";
 import Autocomplete from "../components/Autocomplete";
 import executeQuery from "../db/db";
 import { AutocompleteType, Song } from "../db/db.types";
+import BaseDialog from "../components/dialogs/BaseDialog";
+import { Fragment } from "react";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const result = (await executeQuery({
@@ -34,90 +36,97 @@ type QueryResult = {
 
 const Home = ({ song, autocompleteData }: QueryResult) => {
   return (
-    <div className="h-screen flex flex-col text-center bg-zinc-900">
-      <Head>
-        <title>Oirdle</title>
-        <link rel="icon" href="/favicon.jpg" />
-      </Head>
-      <div className="border-b border-white border-solid w-screen h-16">
-        <div className="dark flex justify-center grow">
-          <div className="flex justify-between max-w-screen-sm w-full mx-auto pr-3 pl-3">
-            <div className="pt-5 pb-5 pl-2 pr-2">
-              <InfoIcon />
-            </div>
-            <div className="pt-5 pb-5 pl-2 pr-2">
-              <HeartIcon />
-            </div>
-            <div className="grow text-center m-auto">
-              <span className="font-seriffont text-4xl text-white">Oirdle</span>
-            </div>
-            <div className="pt-5 pb-5 pl-2 pr-2">
-              <StatsIcon />
-            </div>
-            <div className="pt-5 pb-5 pl-2 pr-2">
-              <HelpIcon />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/*Guesses*/}
-      <div className="flex justify-center h-screen">
-        <div className="flex flex-col justify-between w-full h-full p-3 mx-auto overflow-auto max-w-screen-sm">
-          <div className="flex-col pt-3 items-evenly">
-            <div className="flex items-center p-2 mb-2 border border-custom-mg last:mb-0 border-custom-line">
-              <div className="w-5 h-5"></div>{" "}
-            </div>
-            <div className="flex items-center p-2 mb-2 border last:mb-0">
-              <div className="w-5 h-5"></div>{" "}
-            </div>
-            <div className="flex items-center p-2 mb-2 border last:mb-0">
-              <div className="w-5 h-5"></div>{" "}
-            </div>
-            <div className="flex items-center p-2 mb-2 border last:mb-0">
-              <div className="w-5 h-5"></div>{" "}
-            </div>
-            <div className="flex items-center p-2 mb-2 border last:mb-0">
-              <div className="w-5 h-5"></div>{" "}
-            </div>
-            <div className="flex items-center p-2 mb-2 border last:mb-0">
-              <div className="w-5 h-5"></div>{" "}
-            </div>
-          </div>{" "}
-        </div>
-      </div>
-
-      {/*Song Player*/}
-      <PlayerWidget />
-
-      {/*ProgressBar*/}
-      <div className="flex flex-col justify-center pt-5">
-        <div className="flex flex-col h-28">
-          <div className="flex flex-col border-t border-b border-white">
-            <div className="flex flex-col justify-between w-full h-full pl-3 pr-3 mx-auto overflow-auto max-w-screen-sm">
-              <div style={{ backgroundColor: "#2d2d33" }} className="flex h-4">
-                <div className="w-12 h-full border-r border-white"></div>
-                <div className="w-16 h-full border-r border-white"></div>
-                <div className="w-24 h-full border-r border-white"></div>
-                <div className="h-full border-r border-white w-36"></div>
-                <div className="h-full border-r border-white w-44"></div>
-                <div className="h-full w-60"></div>
+    <Fragment>
+      <BaseDialog isOpen={true} setIsOpen={() => null}></BaseDialog>
+      <div className="flex flex-col h-screen text-center bg-zinc-900">
+        <Head>
+          <title>Oirdle</title>
+        </Head>
+        <div className="w-screen h-16 border-b border-white border-solid">
+          <div className="flex justify-center dark grow">
+            <div className="flex justify-between w-full pl-3 pr-3 mx-auto max-w-screen-sm">
+              <div className="pt-5 pb-5 pl-2 pr-2">
+                <InfoIcon />
+              </div>
+              <div className="pt-5 pb-5 pl-2 pr-2">
+                <HeartIcon />
+              </div>
+              <div className="m-auto text-center grow">
+                <span className="font-seriffont text-4xl text-white">
+                  Oirdle
+                </span>
+              </div>
+              <div className="pt-5 pb-5 pl-2 pr-2">
+                <StatsIcon />
+              </div>
+              <div className="pt-5 pb-5 pl-2 pr-2">
+                <HelpIcon />
               </div>
             </div>
           </div>
+        </div>
 
-          <div className="flex flex-col justify-between w-full h-full pl-3 pr-3 mx-auto overflow-auto max-w-screen-sm">
+        {/*Guesses*/}
+        <div className="flex justify-center h-screen">
+          <div className="flex flex-col justify-between w-full h-full p-3 mx-auto overflow-auto max-w-screen-sm">
             <div className="flex-col pt-3 items-evenly">
               <div className="flex items-center p-2 mb-2 border border-custom-mg last:mb-0 border-custom-line">
-                <div className="w-full">
-                  <Autocomplete options={autocompleteData} />
+                <div className="w-5 h-5"></div>{" "}
+              </div>
+              <div className="flex items-center p-2 mb-2 border last:mb-0">
+                <div className="w-5 h-5"></div>{" "}
+              </div>
+              <div className="flex items-center p-2 mb-2 border last:mb-0">
+                <div className="w-5 h-5"></div>{" "}
+              </div>
+              <div className="flex items-center p-2 mb-2 border last:mb-0">
+                <div className="w-5 h-5"></div>{" "}
+              </div>
+              <div className="flex items-center p-2 mb-2 border last:mb-0">
+                <div className="w-5 h-5"></div>{" "}
+              </div>
+              <div className="flex items-center p-2 mb-2 border last:mb-0">
+                <div className="w-5 h-5"></div>{" "}
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+
+        {/*Song Player*/}
+        <PlayerWidget />
+
+        {/*ProgressBar*/}
+        <div className="flex flex-col justify-center pt-5">
+          <div className="flex flex-col h-28">
+            <div className="flex flex-col border-t border-b border-white">
+              <div className="flex flex-col justify-between w-full h-full pl-3 pr-3 mx-auto overflow-auto max-w-screen-sm">
+                <div
+                  style={{ backgroundColor: "#2d2d33" }}
+                  className="flex h-4"
+                >
+                  <div className="w-12 h-full border-r border-white"></div>
+                  <div className="w-16 h-full border-r border-white"></div>
+                  <div className="w-24 h-full border-r border-white"></div>
+                  <div className="h-full border-r border-white w-36"></div>
+                  <div className="h-full border-r border-white w-44"></div>
+                  <div className="h-full w-60"></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-between w-full h-full pl-3 pr-3 mx-auto overflow-auto max-w-screen-sm">
+              <div className="flex-col pt-3 items-evenly">
+                <div className="flex items-center p-2 mb-2 border border-custom-mg last:mb-0 border-custom-line">
+                  <div className="w-full">
+                    <Autocomplete options={autocompleteData} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
